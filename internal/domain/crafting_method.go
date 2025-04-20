e572467239ca20cec9b022700086d157cf8a7093
Miho Daniel <miho.daniel@mdy.hu>
2025-04-20 14:27:19 +0200

    Add slug field to CraftingMethod struct

--- internal/domain/crafting_method.go
+++ internal/domain/crafting_method.go
@@ -6,6 +6,7 @@ import "time"
 type CraftingMethod struct {
 	ID          int            `db:"id" json:"id"`
 	Name        string         `db:"name" json:"name"`
+	Slug        string         `db:"slug" json:"slug"`
 	Description JSONNullString `db:"description" json:"description"`
 	CreatedAt   time.Time      `db:"created_at" json:"created_at"`
 	UpdatedAt   time.Time      `db:"updated_at" json:"updated_at"`
